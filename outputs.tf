16cd1766df569ad197858b310d48ae49097de502
Jonathan <jonathan.m.lucena@gmail.com>
2022-08-20 20:35:32 -0300

    feat: tf output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,19 @@
 //
 // Run terraform-doc markdown . > README.md
 // Define if needed
+
+output "helm_app_version" {
+    value = helm_release.terraform-k8s-datadog.app_version
+}
+
+output "helm_version" {
+    value = helm_release.terraform-k8s-datadog.version
+}
+
+output "helm_revision" {
+    value = helm_release.terraform-k8s-datadog.revision
+}
+
+output "helm_name" {
+    value = helm_release.terraform-k8s-datadog.name
+}
